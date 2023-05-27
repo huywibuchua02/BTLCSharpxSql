df11bb74030cc2f86876eec308b500c8aa4f7de5
huycoijz <K205480106055@tnut.edu.vn>
2023-05-27 07:38:33 +0700

    ket noi sql

---
+++ connect.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace BTLCSharpxSql
+{
+    internal class connect
+    {
+        //chuỗi kết nối
+        private static string stringConncect = @"Data Source=HUYBU;Initial Catalog=QuanLyBanHang;Integrated Security=True";
+        //kết nối mở csdl
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(stringConncect);
+        }
+    }
+
+}
