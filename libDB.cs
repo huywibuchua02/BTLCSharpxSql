465d0f251eeb303fdcef91c4dca05bd40c62fbbe
huywibuchua02 <K205480106055@tnut.edu.vn>
2023-05-29 23:39:02 +0700

    nnn

--- libDB.cs
+++ libDB.cs
@@ -12,10 +12,6 @@ namespace BTLCSharpxSql
     {
         private string ConnectionString;
 
-        /// <summary>
-        /// Hàm tạo 1 tham số
-        /// </summary>
-        /// <param name="ConnectionString">Đây là chuỗi kết nối tới SQL Server</param>
         public libDB(string ConnectionString)
         {
             this.ConnectionString = ConnectionString;
@@ -28,11 +24,7 @@ namespace BTLCSharpxSql
             return cmd;
 
         }
-        /// <summary>
-        /// hàm thực thi sql đặt trong OleDbCommand
-        /// </summary>
-        /// <param name="cm">đây là đối tượng chứa sql hoặc sp_</param>
-        /// <returns>trả về bảng dữ liệu</returns>
+
         public DataTable Query(SqlCommand cm)
         {
             using (SqlConnection cn = new SqlConnection(ConnectionString))
@@ -46,11 +38,7 @@ namespace BTLCSharpxSql
                 return dt;  //trả về kq
             }
         }
-        /// <summary>
-        /// thực thi câu lệnh sql
-        /// </summary>
-        /// <param name="sql">SQL dạng select</param>
-        /// <returns>Trả về bảng dữ liệu</returns>
+
         public DataTable Query(string sql)
         {
             //sử dụng cấu trúc điều khiển USING để giải phóng đối tượng cm sau khi kết thúc khối USING
@@ -59,11 +47,7 @@ namespace BTLCSharpxSql
                 return Query(cm);
             }
         }
-        /// <summary>
-        /// thực thi 1 sql không trả về dữ liệu: INSERT, UPDATE, DELETE, SP_làm các việc đó
-        /// </summary>
-        /// <param name="cm">OleDbCommand</param>
-        /// <returns>Trả về số lượng bản ghi bị tác động</returns>
+
         public int RunSQL(SqlCommand cm)
         {
             using (SqlConnection cn = new SqlConnection(ConnectionString))
@@ -73,11 +57,7 @@ namespace BTLCSharpxSql
                 return cm.ExecuteNonQuery();//thực thi SQL -> trả về số dòng bị tác động
             }
         }
-        /// <summary>
-        /// thực thi 1 sql loại không trả về dữ liệu INSERT, UPDATE, DELETE
-        /// </summary>
-        /// <param name="sql">SQL trực tiếp, không chứa tham số</param>
-        /// <returns>Trả về số lượng bản ghi bị tác động</returns>
+
         public int RunSQL(string sql)
         {
             using (SqlCommand cm = new SqlCommand(sql))
@@ -85,12 +65,6 @@ namespace BTLCSharpxSql
                 return RunSQL(cm);
             }
         }
-
-        /// <summary>
-        /// thực thi cm
-        /// </summary>
-        /// <param name="cm">cm chứa sql hoặc sp_, có thể có tham số</param>
-        /// <returns>Trả về giá trị trong dòng 1 cột 1</returns>
         public object Scalar(SqlCommand cm)
         {
             using (SqlConnection cn = new SqlConnection(ConnectionString))
@@ -100,11 +74,7 @@ namespace BTLCSharpxSql
                 return cm.ExecuteScalar();
             }
         }
-        /// <summary>
-        /// thực thi SQL
-        /// </summary>
-        /// <param name="sql">SQL trực tiếp, không chứa tham số</param>
-        /// <returns>Trả về giá trị trong dòng 1 cột 1</returns>
+
         public object Scalar(string sql)
         {
             using (SqlCommand cm = new SqlCommand(sql))
